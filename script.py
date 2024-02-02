48ffe2bdfcb91811e8007cd619c3e1ae9d6fba26
Anton Korol <korol142@gmail.com>
2024-02-02 14:43:53 +0300

    Add counter

--- script.py
+++ script.py
@@ -1,10 +1,16 @@
 A =  float(input('Введите число: '))
 n = float(input('Введите степень корня: '))
-Xk = A
-Xk1 = 1
+Xk = 0
+Xk1 = 2
+Xk0 = 0
+counter = 0
 
 while round(Xk, 4) != round(Xk1, 4):
+  
     Xk = Xk1
     Xk1 = (1/n)*((n-1)*Xk + (A/(Xk**(n-1))))
+    counter += 1
+    #print(Xk, Xk1)
 
 print('Корень {0} степени из {1} равен {2}'.format(n, A, round(Xk1, 4)))
+print('Число итераций: {0}'.format(counter))
